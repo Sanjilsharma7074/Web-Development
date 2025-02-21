96fa072697519b5db41f5c5f59bad75b83dd3e21
Sanjilsharma7074 <sanjilsharma456@gmail.com>
2025-02-21 22:39:31 +0530

    creating and sending new data

--- nodeJS/08_Rest_APIs/index.js
+++ nodeJS/08_Rest_APIs/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
+
+
+
+//Middleware - assume it as a plugin 
+// A plugin is like an extra tool or an add-on that you attach to something to give it more features or power.
+// Real-Life Example:
+// You have a basic mobile phone , but you want better photos.
+// You add a special camera lens attachment.
+// → That lens is like a plugin. It improves your phone's camera but your phone still works without it.
+app.use(express.urlencoded({extended:false}));
+//this plugin will help the form data append into the body.
+
+//action of plugin => took the form data->created its js object->appended into req.body
+
+
 
 app
   .route("/api/users/:id")
@@ -18,9 +34,16 @@ app
     return res.json({ status: "pending" });
   });
 
-  app.post("/api/users",(req, res) => {
-    return res.json({ status: "pending" });
-  });
+app.post("/api/users",(req,res)=>{
+  const body = req.body;
+  users.push({...body , id: users.length+1});
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users) , (err,data) =>{
+    return res.json({ Status: "Success" });
+  })
+  
+})
+
+
 
 
 // Routes
