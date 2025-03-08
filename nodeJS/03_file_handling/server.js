7b65f0efdf7119861ba7aade94ebaf4d38a291cb
Sanjilsharma7074 <sanjilsharma456@gmail.com>
2025-03-08 11:23:50 +0530

    -

--- nodeJS/03_file_handling/server.js
+++ nodeJS/03_file_handling/server.js
@@ -8,15 +8,8 @@ fs.writeFile('./async.txt','i am asynchronous', (err,data) => {
 
 })
 
-
-//synchronous => can give us a result by storing the returned value inside a variable.
-// console.log("Before");
-
-const result = fs.readFileSync('./name.txt','utf-8')
-console.log(result);
-
-
-//asynchronous => does not return the result but takes a callback function.
+ 
+//asynchronous => need not have to return the result in a variable instead takes a callback function.
 fs.readFile('./name.txt','utf-8',(err,result) => {
     if(err){
         console.log("Error: ",err);
@@ -27,39 +20,47 @@ fs.readFile('./name.txt','utf-8',(err,result) => {
 })
 
 
+//synchronous => can give us a result by storing the returned value inside a variable.
+console.log("Before");
 
-// console.log("After");
+const result = fs.readFileSync('./name.txt','utf-8')
+console.log(result);
+
+
+
+console.log("After");
 
 fs.appendFileSync('./name.txt','\nHey hello whatsup!');
-// fs.readFile('./name.txt','utf-8', (err,result) => {
-//     if(err){
-//         console.log("Error: ",err);
-//     }
-//     else{
-//         console.log(result);
-//     }
-// })
+const result2 = fs.readFileSync("./name.txt", "utf-8");
+console.log(result2);
 
-// Before
-// hey there, myself sanjil
-// After
-// hey there, myself sanjil
-// Hey hello whatsup!
+
+// !Before
+// ?hey there, myself sanjil
+
+// !After
+// ?hey there, myself sanjil
+// ?Hey hello whatsup!
 
 
 
-//can copy the content of one file to another file also
+// *can copy the content of one file to another file also
 fs.cpSync('./name.txt','./copy.txt')
 
-//to delete a file
-fs.unlinkSync('./copy.txt')
+
+// **to delete a file
+// fs.unlinkSync('./copy.txt')
 
 
-//to check the status of a file.
+// !!to check the status of a file.
+// ? to check the statistics of the file.
+
 console.log(fs.statSync('./async.txt'));
 console.log(fs.statSync('./async.txt').isFile()); //true
 
 
-//to make a directory and many other folders within it.
+
+// ?to make a directory and many other folders within it.
+
 fs.mkdirSync("my-docs/a/b",{recursive:true})
 
